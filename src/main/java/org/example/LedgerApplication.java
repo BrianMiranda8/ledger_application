67140747613f04ac5c178e886745271939ea281c
Brian Miranda <brianmirandamontiel@gmail.com>
2026-05-01 14:00:36 -0700

    fixed pom file

--- src/main/java/org/example/LedgerApplication.java
+++ src/main/java/org/example/LedgerApplication.java
@@ -1,14 +1,6 @@
 package org.example;
 
-import org.example.enums.PaymentType;
-import org.example.models.Ledger;
-import org.example.models.Transaction;
-import org.example.models.TransactionsRepository;
 import org.example.views.HomeView;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 
 public class LedgerApplication {
     static void main() {
